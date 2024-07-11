30e68816d5b4bd87620f81bbeafeafaa28996d1a
Artem Blokhin <rtmdragee@gmail.com>
2024-07-11 23:19:13 +0400

    Added allure

--- src/test/java/ru/netology/test/Pattern.java
+++ src/test/java/ru/netology/test/Pattern.java
@@ -1,6 +1,10 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataGenerator;
@@ -12,8 +16,14 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Pattern {
-
-
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @Test
     public void shouldBeSuccessfullyCompleted() {
         var validUser = DataGenerator.Registration.generateUser("ru");
